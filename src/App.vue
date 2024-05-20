87d7a928cb04b3dff6a10ce737f594a4d4d4e7ea
Ramiro Brescia <ramirobrescia@gmail.com>
2024-05-20 03:13:39 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Start from './components/Start.vue'
+</script>
+
+<template>
+  <Start />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Start.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const ckanURL = ref('')
+</script>
+
+<template>
+  <v-form v-model="valid">
+    <v-container class="mx-auto">
+      <v-row dense>
+        <v-col cols="12">
+          <p>Write a public CKAN api url to view</p>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field v-model="ckanURL" label="CKAN api url" variant="solo" required></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-btn class="text-none" color="primary" size="x-large" variant="flat" block>
+            Explore
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<style scoped></style>
