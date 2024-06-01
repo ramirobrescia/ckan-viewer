dc41dac4306504df7d082faa0de711c72f0dbbca
Ramiro Brescia <ramirobrescia@gmail.com>
2024-06-01 01:45:50 -0300

    CKANOrganizationList component

---
+++ src/components/CKANOrganizationItem.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+	id: String,
+	title: String,
+	name: String
+})
+</script>
+
+<template>
+	<v-list-item :key="id" :title="title"></v-list-item>
+</template>
+
+<style scoped></style>
