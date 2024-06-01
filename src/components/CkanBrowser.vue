fe2c02b2490d2711f3cec1f29ea54b52159c4392
Ramiro Brescia <ramirobrescia@gmail.com>
2024-06-01 01:51:26 -0300

    First organizations fetch from an api

--- src/components/CkanBrowser.vue
+++ src/components/CkanBrowser.vue
@@ -1,33 +1,45 @@
 <script setup>
-import { ref, defineProps, reactive } from 'vue';
-import CKANOrganization from './CKANOrganization.vue';
+import { ref, reactive } from 'vue';
+import CKANOrganizationList from './CKANOrganizationList.vue';
 
 defineProps([
   'url'
 ]);
 
-const organization = reactive({
-	id: '12313',
-	name: 'agricultura',
-	title: 'Ministerio de agricultura',
+const ckanData = reactive({
+	organizations: [
+		{
+			id: '12313',
+			name: 'agricultura',
+			title: 'Ministerio de agricultura',
+		},
+		{
+			id: '4444',
+			name: 'produccion',
+			title: 'Ministerio de producción',
+		},
+	],
 })
 
 const count = ref(0)
 
-function tecnologia(){
+function loadOrganizations(){
 
-	console.log('En tecnologia()');
-	count.value++;
-	organization.title = 'Tecnologia ' + count.value;
-	organization.name = 'tecnologia ' + count.value;
+  fetch('/ckan/action/organization_list?order_by=title&all_fields=True')
+  .then(response => response.json())
+  .then(data => {
+	ckanData.organizations = data.result
+	})
+  .catch(error => console.error(error));
 }
+
 </script>
 
 <template>
-    <h1>Browsing this {{ url }} </h1>
+    <h1>Browsing thisk {{ url }} </h1>
 	<div>
 		<v-btn class="text-none" color="primary" size="x-large" variant="flat" block
-            @click="tecnologia">Tecnologia</v-btn>
+            @click="loadOrganizations">Tecnologia</v-btn>
 	</div>
-	<CKANOrganization :organization="organization" />
+	<CKANOrganizationList :organizations="ckanData.organizations" />
 </template>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    cors: true,
+    proxy: {
+      // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+      '/foo': 'http://localhost:4567',
+      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+      '/ckan': {
+        target: 'https://datos.gob.ar/api/3',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ckan/, ''),
+      },
+    }
+  }
 })
